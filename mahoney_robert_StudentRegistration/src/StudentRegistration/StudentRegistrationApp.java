2901a9bda3e5f152a6359ccd4a879df48597ee56
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-09-05 09:30:57 -0400

    first

---
+++ mahoney_robert_StudentRegistration/src/StudentRegistration/StudentRegistrationApp.java
@@ -0,0 +1,27 @@
+package StudentRegistration;
+
+import java.util.Scanner;
+
+public class StudentRegistrationApp {
+
+	public static void main(String[] args) {
+		System.out.println("Student Registration Form\n");
+		
+		// create a Scanner object named sc
+        @SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter first name: ");
+		String fname = sc.next();
+		System.out.print("Enter last name: ");
+		String lname = sc.next();
+		System.out.print("Enter year of birth: ");
+		int year = sc.nextInt();
+		
+		System.out.println("\nWelcome " + fname + " " + lname + "!");
+		System.out.println("Your Registration is complete.");
+		System.out.println("Your temporary password is: " + fname + "*" + year);
+
+	}
+
+}
