260c99dff52f83b73e4554eaf9102f6480bd5233
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-09-10 19:19:17 -0400

    first

---
+++ Ch07_proj_02_GradeConverter/src/gconvert/Grade.java
@@ -0,0 +1,49 @@
+package gconvert;
+
+public class Grade {
+	private static int number;
+	private static String letter;
+	
+	public Grade() {
+		number = 0;
+	}
+	
+	public Grade(int number) {
+		Grade.number = number;
+		if (number >= 88 && number <= 100) {
+			letter = "A"; 	
+		} else if (number >= 80) {
+			letter = "B";
+		} else if (number >=67) {
+			letter = "C";
+		} else if (number >= 60) {
+			letter = "D";
+		} else if (number <= 59) {
+			letter = "F";
+		} else { 
+			letter = "Not a Valid grade range!";
+		}
+	}
+
+	/**
+	 * @return the number
+	 */
+	public int getNumber() {
+		return number;
+	}
+
+	/**
+	 * @return the letter
+	 */
+	public String getLetter() {
+		return letter;
+	}
+
+	/**
+	 * @param number the number to set
+	 */
+	public void setNumber(int number) {
+		Grade.number = number;
+	}
+
+}
