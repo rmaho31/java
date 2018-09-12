614598f8816253a7bf216a96cc39cd6dff6f6454
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-09-12 15:53:40 -0400

    changed ranges/ inputs

--- Ch02_proj_02_GradeConverter/src/gradeconverter/GradeConverterApp.java
+++ Ch02_proj_02_GradeConverter/src/gradeconverter/GradeConverterApp.java
@@ -12,8 +12,7 @@ public class GradeConverterApp {
 		String choice = "y" ;
 		while (choice.equalsIgnoreCase("y"))
 		{
-			System.out.print("Enter numerical grade: ");
-			int grade = sc.nextInt();
+			int grade = getIntWithinRange(sc, "Enter numerical grade: ", 0, 100);
 			String letter;
 			
 			if (grade >= 88 && grade <= 100)
@@ -29,11 +28,59 @@ public class GradeConverterApp {
 			else 
 				letter = "Not a Valid grade range!";
 			
-			System.out.println("Letter grade: " + letter + "\n");
-			System.out.print("Continue? (y/n): ");
-			choice = sc.next();
-			System.out.println();
+			System.out.println("Letter grade: " + letter);
+			
+			choice = yOrNo(sc, "\nContinue? (y/n): ");
 		}
 		sc.close();
 	}
+	
+	private static String yOrNo(Scanner sc, String prompt) {
+		String choice = "";
+		while (true) {
+			System.out.print(prompt);
+			choice = sc.nextLine();
+			if (choice.equalsIgnoreCase("y") || choice.equalsIgnoreCase("n")) {
+				break;
+			} else {
+				System.out.println("\nError! Must enter y or n. Try again.");
+			}
+		}
+		System.out.println();
+		return choice;
+	}	
+	
+	public static int getIntWithinRange(Scanner sc, String prompt, int min, int max) {
+        int i = 0;
+        boolean isValid = false;
+        while (isValid == false) {
+            i = getInt(sc, prompt);
+            if (i <= min) {
+                System.out.println(
+                        "\nError! Number must be greater than " + min + ".\n");
+            } else if (i >= max) {
+                System.out.println(
+                        "\nError! Number must be less than " + max + ".\n");
+            } else {
+                isValid = true;
+            }
+        }
+        return i;
+    }
+
+	public static int getInt(Scanner sc, String prompt) {
+        int i = 0;
+        boolean isValid = false;
+        while (isValid == false) {
+            System.out.print(prompt);
+            if (sc.hasNextInt()) {
+                i = sc.nextInt();
+                isValid = true;
+            } else {
+                System.out.println("\nError! Invalid integer. Try again.\n");
+            }
+            sc.nextLine();  
+        }
+        return i;
+    }
 }
