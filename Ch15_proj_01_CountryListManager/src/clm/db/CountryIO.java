c2532c2d6bd11acf311b783bf65420540e4594a4
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-09-18 19:25:03 -0400

    first

---
+++ Ch15_proj_01_CountryListManager/src/clm/db/CountryIO.java
@@ -0,0 +1,84 @@
+package clm.db;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountryIO {
+	private List<String> countries = null;
+    private Path countriesPath = null;
+    private File countriesFile = null;
+
+    public CountryIO() {
+        countriesPath = Paths.get("countries.txt");
+        countriesFile = countriesPath.toFile();
+        countries = this.getAll();
+    }
+
+    public List<String> getAll() {
+        // if the countries file has already been read, don't read it again
+        if (countries != null) {
+            return countries;
+        }
+
+        countries = new ArrayList<>();
+
+        if (Files.exists(countriesPath)) { 
+            try (BufferedReader in
+                    = new BufferedReader(
+                            new FileReader(countriesFile))) {
+                // read all countries stored in the file
+                // into the array list
+                String line = in.readLine();
+                while (line != null) {
+                    String country = line;
+
+                    countries.add(country);
+
+                    line = in.readLine();
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+                return null;
+            }
+        }
+        return countries;
+    }
+
+    public String get(int n) {
+    	return countries.get(n);
+    }
+
+    public boolean add(String s) {
+        countries.add(s);
+        return this.saveCountries();
+    }
+
+    public boolean delete(String s) {
+        countries.remove(countries.indexOf(s));
+        return this.saveCountries();
+    }
+
+    private boolean saveCountries() {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedWriter(
+                        new FileWriter(countriesFile)))) {
+            for (String s : countries) {
+                out.print(s + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+            return false;
+        }
+        return true;
+    }
+}
---
+++ Ch15_proj_01_CountryListManager/src/clm/main/CountryListManagerApp.java
@@ -0,0 +1,37 @@
+package clm.main;
+
+import clm.db.CountryIO;
+import clm.utility.Console;
+
+public class CountryListManagerApp {
+
+	public static void main(String[] args) {
+		Console.println("Country List Manager\n");
+		
+		Console.println("COMMAND MENU");
+		Console.println("1 - List countries");
+		Console.println("2 - Add a country");
+		Console.println("3 - Delete a country");
+		Console.println("4 - Exit");
+		
+		int choice = 0;
+		while (choice != 4) {
+			CountryIO cio = new CountryIO();
+			choice = Console.getInt("Enter menu number: ", 0, 5);
+			
+			if(choice == 1) {
+				System.out.println();
+				for(String s : cio.getAll()) {
+					System.out.println(s);
+				}
+			} else if (choice == 2) {
+				cio.add(Console.getString("Enter country: "));
+			} else if (choice == 3) {
+				cio.delete(Console.getString("Enter country: "));
+			}
+		}
+		
+
+	}
+
+}
