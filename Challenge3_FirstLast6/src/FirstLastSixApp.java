6509b657eb77951d45feb348eaf7dd8116b05e85
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-09-18 07:26:14 -0400

    first

---
+++ Challenge3_FirstLast6/src/FirstLastSixApp.java
@@ -0,0 +1,27 @@
+
+public class FirstLastSixApp {
+
+	public static void main(String[] args) {
+		int[] n1 = {1,2,3,4,5,6,};
+		int[] n2 = {6,5,4,3,2,1};
+		int[] n3 = {1,2,6};
+		int[] n4 = {6,1,2,3};
+		int[] n5 = {13,6,1,2,3};
+
+		System.out.println(firstLast6(n1)); // true
+		System.out.println(firstLast6(n2)); // true
+		System.out.println(firstLast6(n3)); // true
+		System.out.println(firstLast6(n4)); // true
+		System.out.println(firstLast6(n5)); // true
+	}
+	
+	public static boolean firstLast6(int[] nums) {
+		if(nums[0] == 6) {
+			return true;
+		} else if (nums[nums.length-1] == 6) {
+			return true;
+		}
+		return false;
+	}
+
+}
