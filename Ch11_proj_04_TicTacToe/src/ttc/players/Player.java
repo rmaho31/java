13a95276b51da5b484d0f50c60e979fae3e80d7d
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-09-21 08:56:44 -0400

    updated

--- Ch11_proj_04_TicTacToe/src/ttc/players/Player.java
+++ Ch11_proj_04_TicTacToe/src/ttc/players/Player.java
@@ -2,16 +2,10 @@ package ttc.players;
 
 public class Player {
 	private String letter;
-	private int[] rowSums;
-	private int[] diagonalSums;
-	private int[] columnSums;
 	
 	
 	public Player(String letter, int size) {
-		this.letter = letter;
-		rowSums = new int[size];
-		diagonalSums = new int[2];
-		columnSums = new int[size];		
+		this.letter = letter;	
 	}
 
 	public String getLetter() {
@@ -22,39 +16,4 @@ public class Player {
 		this.letter = letter;
 	}
 
-	public int[] getRowSums() {
-		return rowSums;
-	}
-
-	public void setRowSums(int[] rowSums) {
-		this.rowSums = rowSums;
-	}
-
-	public int[] getDiagonalSums() {
-		return diagonalSums;
-	}
-
-	public void setDiagonalSums(int[] diagonalSums) {
-		this.diagonalSums = diagonalSums;
-	}
-
-	public int[] getColumnSums() {
-		return columnSums;
-	}
-
-	public void setColumnSums(int[] columnSums) {
-		this.columnSums = columnSums;
-	}
-
-	public void incrementColumnSums(int index) {
-		columnSums[index]++;
-	}
-	
-	public void incrementRowSums(int index) {
-		rowSums[index]++;
-	}
-	
-	public void incrementDiagonalSums(int index) {
-		diagonalSums[index]++;
-	}
 }
