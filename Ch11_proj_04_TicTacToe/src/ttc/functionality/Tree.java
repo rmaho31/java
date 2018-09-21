773879d8f76b634e8d1260311e3c0340dd3f1046
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-09-21 08:52:22 -0400

    first

---
+++ Ch11_proj_04_TicTacToe/src/ttc/functionality/Tree.java
@@ -0,0 +1,18 @@
+package ttc.functionality;
+
+public class Tree {
+	private Node root;
+	
+	public Tree() {
+		
+	}
+
+	public Node getRoot() {
+		return root;
+	}
+
+	public void setRoot(Node root) {
+		this.root = root;
+	}
+	
+}
